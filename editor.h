95250d0a1d5fad405febdc41ec50b55cf3eb7b98
ElianaNeto <eliananeto29@gmail.com>
2021-05-30 19:06:08 +0100

    First commit

---
+++ editor.h
@@ -0,0 +1,32 @@
+#ifndef EDITOR_H_INCLUDED
+#define EDITOR_H_INCLUDED
+
+#define NOT_FOUND -1
+#define OK 0
+#define LIST_FULL 1
+#define LIST_EMPTY 2
+#define INVALID_INDEX 3
+#define NO_SPACE 4
+#define IMPOSSIBLE_REMOTION 5
+
+#define MAXPASS 5
+#define MAXLISTA 12
+
+typedef enum
+{
+    FALSE = 0,
+    TRUE = 1
+} Boolean;
+
+//Operacoes
+
+//inserir
+//remover
+//linha
+//Localizar
+//alterar
+//ultimo
+//imprimir
+//fim
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "editor.h"
+
+int main()
+{
+    /* code */
+    return 0;
+}
